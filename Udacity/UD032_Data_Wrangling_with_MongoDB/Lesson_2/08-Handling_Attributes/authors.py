a0e3f3f8d300092264372e95d6dfdeba94f824c3
Anirudha Bose <ani07nov@gmail.com>
2015-05-16 10:30:50 +0530

    Lesson 2: Solution for Handling Attributes

--- Udacity/UD032_Data_Wrangling_with_MongoDB/Lesson_2/08-Handling_Attributes/authors.py
+++ Udacity/UD032_Data_Wrangling_with_MongoDB/Lesson_2/08-Handling_Attributes/authors.py
@@ -19,13 +19,14 @@ def get_authors(root):
     authors = []
     for author in root.findall('./fm/bibl/aug/au'):
         data = {
-                "fnm": None,
-                "snm": None,
-                "email": None,
+                "fnm": author.find('fnm').text,
+                "snm": author.find('snm').text,
+                "email": author.find('email').text,
                 "insr": []
         }
 
-        # YOUR CODE HERE
+        for ins in author.findall('insr'):
+            data["insr"].append(ins.attrib["iid"])
 
         authors.append(data)
 
