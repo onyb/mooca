e46206adb6cba270580411235f4501a15cac90ec
Anirudha Bose <ani07nov@gmail.com>
2015-05-19 17:32:19 +0530

    Problem Set 4: Updating Schema

--- Udacity/UD032_Data_Wrangling_with_MongoDB/Lesson_4/Problem_Set/03-Updating_Schema/update.py
+++ Udacity/UD032_Data_Wrangling_with_MongoDB/Lesson_4/Problem_Set/03-Updating_Schema/update.py
@@ -5,29 +5,32 @@ In this problem set you work with another type of infobox data, audit it, clean 
 come up with a data model, insert it into a MongoDB and then run some queries against your database.
 The set contains data about Arachnid class.
 
-The data is already in the database. But you have been given a task to also include 'binomialAuthority'
-information in the data, so you have to go through the data and update the existing entries.
+For this exercise, the arachnid data is already in the database. You have been given the task of
+including 'binomialAuthority' information in the records. You will do this by processing the arachnid.csv to
+extract binomial authority data and then using this data to update the corresponding data base records.
 
 The following things should be done in the function add_field:
 - process the csv file and extract 2 fields - 'rdf-schema#label' and 'binomialAuthority_label'
-- clean up the 'rdf-schema#label' same way as in the first exercise - removing redundant "(spider)" suffixes
-- return a dictionary, with 'label' being the key, and 'binomialAuthority_label' the value
-- if 'binomialAuthority_label' is "NULL", skip the item
+- clean up the 'rdf-schema#label' the same way as in the first exercise - removing redundant "(spider)" suffixes
+- return a dictionary with the cleaned 'rdf-schema#label' field values as keys,
+  and 'binomialAuthority_label' field values as values
+- if 'binomialAuthority_label' is "NULL" for a row in the csv, skip the item
 
 The following should be done in the function update_db:
-- query the database by using the field 'label'
-- update the data, by adding a new item under 'classification' with a key 'binomialAuthority'
+- query the 'label' field in the database using rdf-schema#label keys from the data dictionary
+- update the documents by adding a new item under 'classification' with the key 'binomialAuthority' and the
+  binomialAuthority_label value from the data dictionary as the value
 
+For item {'Argiope': 'Jill Ward'} in the data dictionary, the resulting document structure
+should look like this:
 
-The resulting data should look like this:
-- the output structure should be as follows:
 { 'label': 'Argiope',
   'uri': 'http://dbpedia.org/resource/Argiope_(spider)',
   'description': 'The genus Argiope includes rather large and spectacular spiders that often ...',
   'name': 'Argiope',
   'synonym': ["One", "Two"],
   'classification': {
-                    'binomialAuthority': None,
+                    'binomialAuthority' : 'Jill Ward'
                     'family': 'Orb-weaver spider',
                     'class': 'Arachnid',
                     'phylum': 'Arthropod',
@@ -36,11 +39,15 @@ The resulting data should look like this:
                     'genus': None
                     }
 }
+
+Note that the value in the 'binomialAuthority' field is a placeholder; this is only to
+demonstrate the output structure form, for the entries that require updating.
 """
 import codecs
 import csv
 import json
 import pprint
+import re
 
 DATAFILE = 'arachnid.csv'
 FIELDS ={'rdf-schema#label': 'label',
@@ -48,29 +55,35 @@ FIELDS ={'rdf-schema#label': 'label',
 
 
 def add_field(filename, fields):
-
     process_fields = fields.keys()
     data = {}
     with open(filename, "r") as f:
         reader = csv.DictReader(f)
         for i in range(3):
             l = reader.next()
-        # YOUR CODE HERE
+
+        for line in reader:
+            # trim out redundant description in parenthesis from the 'rdf-schema#label' field, like "(spider)"
+            line['rdf-schema#label'] = re.sub(r"\(.*\)", "", line['rdf-schema#label']).strip()
+
+            if line['binomialAuthority_label'] != "NULL":
+                data[line['rdf-schema#label']] = line['binomialAuthority_label']
 
     return data
 
 
 def update_db(data, db):
-    # YOUR CODE HERE
-    pass
-
+    for key in data:
+        db.arachnid.update({'label': key},
+                           {'$set': {'classification': {'binomialAuthority': data[key]}}
+                            })
 
 def test():
     # Please change only the add_field and update_db functions!
     # Changes done to this function will not be taken into account
     # when doing a Test Run or Submit, they are just for your own reference
     # and as an example for running this code locally!
-    
+
     data = add_field(DATAFILE, FIELDS)
     from pymongo import MongoClient
     client = MongoClient("mongodb://localhost:27017")
