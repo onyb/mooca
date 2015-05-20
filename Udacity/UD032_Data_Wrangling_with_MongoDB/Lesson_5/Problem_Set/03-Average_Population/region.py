c111369ffdb9d9f0d8cdecbdb7d38d42443f9e21
Anirudha Bose <ani07nov@gmail.com>
2015-05-20 18:31:06 +0530

    Problem Set 5: Average Population

--- Udacity/UD032_Data_Wrangling_with_MongoDB/Lesson_5/Problem_Set/03-Average_Population/region.py
+++ Udacity/UD032_Data_Wrangling_with_MongoDB/Lesson_5/Problem_Set/03-Average_Population/region.py
@@ -32,7 +32,24 @@ def get_db(db_name):
 
 def make_pipeline():
     # complete the aggregation pipeline
-    pipeline = [ ]
+    pipeline = [
+                {'$unwind': '$isPartOf'},
+
+                {'$group': {
+                                '_id': {
+                                            'region': '$isPartOf',
+                                            'country': '$country'
+                                       },
+                                'avgr': {'$avg': '$population'}
+                           }
+                },
+
+                {'$group': {
+                                '_id': '$_id.country',
+                                'avgRegionalPopulation': {'$avg': '$avgr'}
+                           }
+                }
+               ]
     return pipeline
 
 def aggregate(db, pipeline):
