b56b8a30e106b13f046e62225a5865338e1d88fc
Anirudha Bose <ani07nov@gmail.com>
2015-09-09 15:40:09 +0530

    CS344: Thread Block Programming Example

---
+++ Udacity/CS344_Intro_to_Parallel_Programming/Lesson_2/18-A_Programming_Example_on_Blocks/example.cu
@@ -0,0 +1,31 @@
+/*
+ * Code for a Thread Block Programming Example
+
+ * Author: Anirudha Bose <ani07nov@gmail.com>
+ * Part of CS344: Intro to Parallel Programming
+ * http://github.com/onyb/mooca
+ */
+
+#include <stdio.h>
+
+#define NUM_BLOCKS 16
+#define BLOCK_WIDTH 1
+
+__global__ void hello()
+{
+    printf("Hello world! I'm a thread in block %d\n", blockIdx.x);
+}
+
+
+int main(int argc,char **argv)
+{
+    // launch the kernel
+    hello<<<NUM_BLOCKS, BLOCK_WIDTH>>>();
+
+    // force the printf()s to flush
+    cudaDeviceSynchronize();
+
+    printf("That's all!\n");
+
+    return 0;
+}
